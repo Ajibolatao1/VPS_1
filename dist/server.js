e0a15f98e917fc91d2d616925797f74597d95732
Ajibolatao1 <123806876+Ajibolatao1@users.noreply.github.com>
2024-10-20 19:44:02 +0800

    Trying to reploy

--- dist/server.js
+++ dist/server.js
@@ -5,11 +5,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
-console.log(process.env.PORT);
 app.get("/", (req, res) => {
     res.send({ message: "Hello World" });
 });
 // Start Server
-app.listen(process.env.PORT, () => {
+app.listen(5000, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
 });
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,11 @@
 import express from "express";
 const app = express();
 
-console.log(process.env.PORT);
-
 app.get("/", (req, res) => {
   res.send({ message: "Hello World" });
 });
 
 // Start Server
-app.listen(process.env.PORT, () => {
+app.listen(5000, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
 });
